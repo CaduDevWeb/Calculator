67bbe9d8d33c849f4ed098d1907456ef49674c35
CaduDevWeb <carloseduardo.casotti@outlook.com>
2025-07-04 17:32:50 -0300

    reformulacao para aceitar contas com multiplos operadores

--- script.js
+++ script.js
@@ -38,41 +38,53 @@ function buttonClear(){
 function resultEquation(){
     let equationStrings = displayScreen.textContent;
     const operators = ['+','-','/','*','%'];
-    let operation = null;
-    let operatorIndex = -1;
+    let operation = [];
+    let operatorIndex = [];
 
     console.log("2. Operadores definidos:", operators);
+    // 25+5+4/2
     // achar o primeiro operador 
     for(let i=0; i < equationStrings.length;i++){
         const char = equationStrings[i];
         if(operators.includes(char)){
-            operation = char;
-            operatorIndex = i;
-            break;
+            operation.push(char);
+            operatorIndex.push(i);
         }
     }
-    // verificar se operador foi encontrado
-    if(operation === null || operatorIndex === -1){
-        displayScreen.innerText = "Operador nao encontrado"
-        return;
-    }
+    console.log(`Operacoes registradas: ${operation}`,`Index das operacoes: ${operatorIndex}`);
+    // verificar se operador foi encontrado(DESATIVADO TEMPORARIAMENTE)
+    // if(operation === null || operatorIndex === -1){
+    //     displayScreen.innerText = "Operador nao encontrado"
+    //     return;
+    // }
 
     // extrair os operandos da string
-    const operand1String = equationStrings.substring(0, operatorIndex);
-    const operand2String = equationStrings.substring(operatorIndex + 1);
-
-    //transformar em numeros 
-    const variable1 = parseFloat(operand1String);
-    const variable2 = parseFloat(operand2String);
-
-
-    // veficar se a extração foi bem-sucedida
-    if(isNaN(variable1) || isNaN(variable2)){
-        displayScreen.innerText = "Conversão de numero mal sucedida"
+    let operands = [];
+    let startIndex = 0;
+    for (a=0; a < operation.length;a++){
+        const currentOperatorIndex  = operatorIndex[a];//{currentoperator:6}
+        const operandString = equationStrings.substring(startIndex, currentOperatorIndex);// 0 2 ||  3 4 || 
+        operands.push(parseFloat(operandString));//{5}
+        startIndex = currentOperatorIndex + 1;//{starIndex:5}
     }
+
+    const lastOperands = equationStrings.substring(startIndex);
+    operands.push(parseFloat(lastOperands));
+    console.log(operands);
+    
+
+    //transformar em numeros(DESATIVADO POIS CONVERTE DIRETO NO LOOP AGORA)
+    // const variable1 = parseFloat(operand1String);
+    // const variable2 = parseFloat(operand2String);
+
+
+    // // veficar se a extração foi bem-sucedida(INUTILIZADO)
+    // if(isNaN(variable1) || isNaN(variable2)){
+    //     displayScreen.innerText = "Conversão de numero mal sucedida"
+    // }
     
     let result = 0;
-
+    //operands{numeros},operation{tiposDeOperacao}
     switch(operation){
         case "%": result = variable1 % variable2;
         break;
